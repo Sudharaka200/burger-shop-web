becbf4de03cb4b34ea42c8e0381a6ac2f5699e10
Sudharaka pathiraja <sudharakadulanjith200@gmail.com>
2024-11-11 02:00:30 +0530

    first commit

---
+++ burger-shop-web/home/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Header files -->
+     <?php include '../library/head.php' ?>
+     <?php include '../library/style.php' ?>
+    <!-- Header files -->
+
+    <!-- navbar -->
+     <?php include '../library/navbar.php' ?>
+    <!-- navbar -->
+
+    <!-- Area 1 -->
+     <div>
+        <div class="container-fuild area-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-5 col-lg-6">
+                        <h1>The best burger <br>
+                        you will ever find!</h1>
+                        <p>For the best burgers in Colombo, look no further than MrBurger to satisfy those belly cravings all day and any day</p>
+                        <button class="btn-nav-order">Order Now</button>
+                    </div>
+                    <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+                       <img src="../images/background-bruger.png" alt="" class="img1-home-area1">
+                    </div>
+                </div>
+                <div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-5 col-lg-6">
+                                <div>
+                                    <h2>Bursting with flavour</h2>
+                                    <p>The brainchild of Ikram Salam, MrBurger puts a delectable twist on the classic burger to bring you fusion appeal that will leave you hungry for more!
+                                        <br>
+                                        From generous lip-smacking portions, to uniting East and West in a medley of flavours, we invite you to dive into our menu and allow us to grill up a feast to feed your heart’s desire!
+                                        <br>
+                                        The best kind of food is one that lets you dig in with your hands; so it’s no wonder the glorious burger ticks off all the boxes as a foodie’s go-to comfort food.</p>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+                                <div>
+                                    <img src="../images/Snapinsta.app_331663968_723783919388684_6380229169994041891_n_1080-1024x853.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+    <!-- Area 1 -->
+    
+</body>
+</html>
---
+++ burger-shop-web/library/style.php
@@ -0,0 +1,10 @@
+<style>
+    /* navbr */
+    .navbar{background-color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+    .btn-nav-order{border-radius: 8px;height: 35px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+
+    /* area1-home */
+    .area-1{background-color: black; color: #fff;}
+    .img1-home-area1{width: 100%;z-index: 100;}
+
+</style>
