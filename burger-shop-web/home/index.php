cc5f0394be8b83705d37ef68f1e53b104f62f0b2
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2024-11-27 00:44:34 +0530

    Add files via upload

--- burger-shop-web/home/index.php
+++ burger-shop-web/home/index.php
@@ -21,10 +21,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-md-5 col-lg-6">
-                        <h1>The best burger <br>
+                        <h1 class="area1-h1 font-italic">The best burger <br>
                         you will ever find!</h1>
-                        <p>For the best burgers in Colombo, look no further than MrBurger to satisfy those belly cravings all day and any day</p>
-                        <button class="btn-nav-order">Order Now</button>
+                        <p class="font-bold area1-home-p">For the best burgers in Colombo, look no further than MrBurger to satisfy those belly cravings all day and any day</p>
+                        <br>
+                        <button class="btn-family">Order Now</button>
                     </div>
                     <div class="col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
                        <img src="../images/background-bruger.png" alt="" class="img1-home-area1">
--- burger-shop-web/library/style.php
+++ burger-shop-web/library/style.php
@@ -1,14 +1,25 @@
 <style>
-    /* navbr */
+@import url('https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Caveat:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&display=swap');    /* navbr */
+@import url('https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Caveat:wght@400..700&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+    /* Theme */
+    .font-italic{font-family: 'Caveat', cursive;}
+    .font-bold{font-family:'Barlow', Helvetica, sans-serif;}
+
+
+
     .navbar{background-color: black; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
-    .btn-nav-order{border-radius: 8px;height: 35px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;}
+    .btn-nav-order{border-radius: 8px;}
+    .btn-family{border-radius: 8px;height: 35px; width: 200px;height: 50px; }
 
     /* area1-home */
     .area-1{background-color: black; color: #fff;}
-    .img1-home-area1{width: 50%;z-index: 100; }
+    .img1-home-area1{width: 100%;z-index: 100;margin-top: 10%;}
     .img2-area1{width: 100%;}
     .div-card1-area1{background-color: #F7F2F2;color: black; height: 100%; width: 100%; border-radius: 20px;text-align: center; box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);}
     .div-card2-area1{height: 100%; width: 100%; border-radius: 20px;box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.3);}
     .div-margin-area1{margin-top: 10%;}
+    .area1-h1{margin-top: 30%; font-size: 70px;}
+    .area1-home-p{font-size: 20px;}
 
 </style>
